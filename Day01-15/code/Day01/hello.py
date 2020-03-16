a7f2119afae90e933ec10f91b5372a661ab4739c
cjh <1322452189@qq.com>
2020-03-16 17:20:48 +0800

    增加day02和day03的例子

--- Day01-15/code/Day01/hello.py
+++ Day01-15/code/Day01/hello.py
@@ -1,3 +1,6 @@
+"""
+ 第一个python程序
+"""
 print('hello, world!')
 print("你好,世界！")
 print('你好', '世界')
---
+++ Day01-15/code/Day02/variable1.py
@@ -0,0 +1,15 @@
+"""
+使用变量保存数据并进行操作
+"""
+
+a = 321
+b = 123
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(a // b)  #取整除数
+print(a % b)   #取余
+print(a ** b)  #a的b次方
